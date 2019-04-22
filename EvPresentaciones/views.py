9256f5fa528bdb5cea9facb71561fec04aaa49c2
terrypls <selvastian@gmail.com>
2019-04-22 19:43:10 -0400

    conectadas paginas con django

--- EvPresentaciones/views.py
+++ EvPresentaciones/views.py
@@ -13,3 +13,50 @@ def testPage(request, value):
 
     #Retornamos el el request, con el html asociado y un diccionario con los parametros que este necesita.
     return render(request, 'EvPresentaciones/testPage.html', {'value': value , 'list': range(1,value)})
+
+#funciones Admin interface
+
+def Cursos_admin(request):
+    return render(request, 'EvPresentaciones/Admin_interface/Cursos_admin.html')
+    
+def Evaluaciones_admin(request):
+    return render(request, 'EvPresentaciones/Admin_interface/Evaluaciones_admin.html')
+
+def Evaluadores_admin(request):
+    return render(request, 'EvPresentaciones/Admin_interface/Evaluadores_admin.html')
+    
+def Landing_page_admin(request):
+    return render(request, 'EvPresentaciones/Admin_interface/Landing_page_admin.html')
+
+def Rubricas_admin(request):
+    return render(request, 'EvPresentaciones/Admin_interface/Rubricas_admin.html')
+
+#funciones Evaluaciones
+
+def Evaluacion(request):
+    return render(request, 'EvPresentaciones/Eval_interface/evaluacion.html')
+
+def Evaluacion_admin(request):
+    return render(request, 'EvPresentaciones/Eval_interface/evaluacionadmin.html')
+
+def Post_evaluacion(request):
+    return render(request, 'EvPresentaciones/Eval_interface/postevaluacion.html')
+
+def Post_evaluaciones_admin(request):
+    return render(request, 'EvPresentaciones/Eval_interface/postevaluacionadmin.html')
+
+#funciones Rubricas
+
+def Ficha_Rubrica_admin(request):
+    return render(request, 'EvPresentaciones/FichasRubricas/FichaRubricaAdministrador.html')
+
+def Ficha_Rubrica_evaluador(request):
+    return render(request, 'EvPresentaciones/FichasRubricas/FichaRubricaEvaluador.html')
+
+#funciones resumen evaluacion
+
+def Auth_summary(request):
+    return render(request,'EvPresentaciones/Summary_student/auth_summary.html')
+
+def Summary(request):
+    return render(request,'EvPresentaciones/Summary_student/summary.html')
